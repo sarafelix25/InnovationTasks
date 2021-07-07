6ac8d80cc84c7f45c2811bb730110c2e52b2a6ab
Sara Felix <sarabfelix1@gmail.com>
2021-07-07 17:25:24 -0300

    autolayout e tasksTableViewController

---
+++ InnovationTasks/Controllers/TasksTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TasksTableViewController.swift
+//  InnovationTasks
+//
+//  Created by sara.batista.d.felix on 07/07/21.
+//
+
+import UIKit
+
+class TasksTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
